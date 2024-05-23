7b0d1cd9c2e2705ae768d18d9bc17723f2ef905c
Ayush30s <ayushsri8318328947@gmail.com>
2024-05-23 14:35:28 +0530

    res slider root added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hellooo");
 });
 
 app.get("/api/restaurants", (req, res) => {
@@ -62,6 +62,33 @@ app.get("/api/restaurants/bodyfirstpart", (req, res) => {
        res.status(500).send("An error occurred");
     });
  });
+
+ app.get("/api/restaurants/bodyfirstsection", (req, res) => {
+   const { lat, lng } = req.query;
+   const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+ 
+    fetch(url, {
+       headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+       },
+    })
+    .then((response) => {
+       if (!response.ok) {
+       throw new Error("Network response was not ok");
+       }
+       return response.json();
+    })
+    .then((data) => {
+       res.json(data);
+    })
+    .catch((error) => {
+       console.error(error);
+       res.status(500).send("An error occurred");
+    });
+ }); 
 
  
 app.listen(3001, () => {
